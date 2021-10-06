7377cf9848b177b9b654c3230e8c7326a64f7880
cultureweb <cedric.milius@yahoo.fr>
2021-10-06 10:25:40 +0200

    first commit

---
+++ src/Controller/AccountPasswordController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UpdatePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountPasswordController extends AbstractController
+{
+    private $entityManager;
+
+    /**
+     * AccountPasswordController constructor.
+     * @param $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/compte/mot-de-passe", name="account_password")
+     */
+    public function index(Request $request, UserPasswordHasherInterface $encoder): Response
+    {
+        $notification = null;
+        $user=$this->getUser();
+        $form=$this->createForm(UpdatePasswordType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $old_pwd=$form->get('old_password')->getData();
+//            dd($old_pwd);
+
+            if ($encoder->isPasswordValid($user, $old_pwd)){
+//         die('ça marche');
+                $new_pwd= $form->get('new_password')->getData();
+                $password=$encoder->hashPassword($user, $new_pwd);
+                $user->setPassword($password);
+                $this->entityManager->flush();
+                $notification = "Votre mot de passe a bien été mis à jour";
+            } else {
+                $notification = "Votre mot de passe est incorrect";
+            }
+        }
+        return $this->render('account/password.html.twig',[
+            'form'=>$form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
